06691af7d4154e04dbd2ca47c09c27105d560ab6
sfareds <sfareds@gmail.com>
2023-03-27 18:45:16 +0300

    refactor:remove duplicate style in skeleton

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -2,6 +2,7 @@ import { Card, CardBody, Heading, HStack, Image, Text } from "@chakra-ui/react";
 import { Game } from "../hooks/useGames";
 import getCroppedImageUrl from "../services/image-url";
 import CriticScore from "./CriticScore";
+import GameCardContainer from "./GameCardContainer";
 import PlatformIconList from "./PlatformIconList";
 
 interface Props {
@@ -10,20 +11,22 @@ interface Props {
 
 const GameCard = ({ game }: Props) => {
   return (
-    <Card width='300px' borderRadius='10' overflow='hidden'>
-      <Image src={getCroppedImageUrl(game.background_image)} />
-      <CardBody>
-        <>
-          <Heading fontSize={"2xl"}>{game.name}</Heading>
-          <HStack justifyContent={"space-between"}>
-            <PlatformIconList
-              platforms={game.parent_platforms.map((p) => p.platform)}
-            ></PlatformIconList>
-            <CriticScore score={game.metacritic}></CriticScore>
-          </HStack>
-        </>
-      </CardBody>
-    </Card>
+    <GameCardContainer>
+      <Card>
+        <Image src={getCroppedImageUrl(game.background_image)} />
+        <CardBody>
+          <>
+            <Heading fontSize={"2xl"}>{game.name}</Heading>
+            <HStack justifyContent={"space-between"}>
+              <PlatformIconList
+                platforms={game.parent_platforms.map((p) => p.platform)}
+              ></PlatformIconList>
+              <CriticScore score={game.metacritic}></CriticScore>
+            </HStack>
+          </>
+        </CardBody>
+      </Card>
+    </GameCardContainer>
   );
 };
 
